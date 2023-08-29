4126e337891d5e2c1bddd8e3b60a33212ace8315
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-08-29 13:53:32 +0300

    Added router.py

---
+++ user/router.py
@@ -0,0 +1,78 @@
+from fastapi import APIRouter, Depends, HTTPException, Form
+from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from config import SECRET_KEY, ALGORITHM
+from dependencies import db_dependency, get_async_session
+from user import schemas, crud, models
+from user.crud import get_user_by_email
+from user.schemas import Token, TokenRefresh, UserResponse
+from user.security import verify_password
+
+
+router = APIRouter(prefix="/api", tags=["user"])
+
+
+@router.post("/user/register/", response_model=schemas.User)
+async def create_user(user: schemas.UserCreate, db: db_dependency):
+    return await crud.create_user(db=db, user=user)
+
+
+@router.post("/user/token/")
+async def login_for_tokens(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: AsyncSession = Depends(get_async_session),
+):
+    user = await get_user_by_email(db, form_data.username)
+
+    if user is None or not verify_password(form_data.password, user.password):
+        raise HTTPException(status_code=400, detail="Invalid email or password")
+
+    access_token, refresh_token = crud.create_tokens(
+        {"user_id": user.id, "sub": user.email}
+    )
+
+    return Token(
+        access_token=access_token, refresh_token=refresh_token, token_type="bearer"
+    )
+
+
+@router.post("/user/refresh-token/")
+async def refresh_token(refresh_token: str = Form(...)):
+    try:
+        payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = payload.get("user_id")
+        email: str = payload.get("sub")
+        if email is None:
+            raise HTTPException(status_code=401, detail="Invalid refresh token")
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Refresh token has expired")
+    except jwt.JWTError:
+        raise HTTPException(status_code=401, detail="Could not validate refresh token")
+
+    user = models.User(id=user_id, email=email)
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    access_token, _ = crud.create_tokens({"user_id": user.id, "sub": user.email})
+
+    return TokenRefresh(access_token=access_token, token_type="bearer")
+
+
+@router.get("/user/me/", response_model=UserResponse)
+async def get_user_profile(current_user: models.User = Depends(crud.get_current_user)):
+    formatted_registered_at = current_user.registered_at.strftime("%Y-%m-%d %H:%M:%S")
+
+    user_response = UserResponse(
+        id=current_user.id,
+        email=current_user.email,
+        username=current_user.username,
+        first_name=current_user.first_name,
+        last_name=current_user.last_name,
+        registered_at=formatted_registered_at,
+        is_active=current_user.is_active,
+        is_superuser=current_user.is_superuser,
+    )
+
+    return user_response
